af1d155153319bff6fa76ec7250db0cb112e39c0
mayankPratap-spec <62495914+mayankPratap-spec@users.noreply.github.com>
2024-05-03 19:41:27 +0530

    Update main.tf

--- CleverTap-Task/main.tf
+++ CleverTap-Task/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 
 module "vpc" {
-  source                     = "./vpc" // Assuming the module is in the same directory
+  source                     = "./vpc" 
   vpc_cidr_block             = "10.0.0.0/16"
   public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
